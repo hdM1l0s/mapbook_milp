bf7d7004459064936fdaf5621e4f50f39c3e5b63
hdM1l0s <u63764@student.wat.edu.pl>
2025-04-08 19:01:14 +0200

    chore: mvt pattern

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-users = [
-    {"name": "Miłosz", "Location": "Warszawa", "posts": 500},
-    {"name": "Michał", "Location": "Krasnystaw", "posts": 400},
-    {"name": "Ksavier", "Location": "Grudziąc", "posts": 300},
-    {"name": "Damian", "Location": "Kraków", "posts": 200},
+from untils.controller import get_user_info
+from untils.models import users
 
-]
+def main():
+    get_user_info(users)
 
 
-def get_user_info(users_data: list)->None:
-    for user in users_data:
-        print(f"Twój znajomy{user['name']}, z miejscowości: {user['Location']} opublikował {user['posts']} postów")
 
-
-get_user_info(users)
+if __name__ == "__main__":
+    main()
---
+++ untils/models.py
@@ -0,0 +1,7 @@
+users = [
+    {"name": "Miłosz", "Location": "Warszawa", "posts": 500},
+    {"name": "Michał", "Location": "Krasnystaw", "posts": 400},
+    {"name": "Ksavier", "Location": "Grudziąc", "posts": 300},
+    {"name": "Damian", "Location": "Kraków", "posts": 200},
+
+]
