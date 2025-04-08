d014be79eefdbe5c64645ea770b5b9088ebffca5
hdM1l0s <u63764@student.wat.edu.pl>
2025-04-08 18:49:33 +0200

    wip: data structure

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ users = [
 
 ]
 
-for user in users:
-    print(f"Twój znajomy{user['name']}, z {user['location']} opublikował {user['posts']} postów")
+
+def get_user_info(users_data: list)->None:
+    for user in users_data:
+        print(f"Twój znajomy{user['name']}, z miejscowości: {user['Location']} opublikował {user['posts']} postów")
+
+
+get_user_info(users)
