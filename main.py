cff6f32370adbe1c1b7294bd947f9b897d3c258c
hdM1l0s <u63764@student.wat.edu.pl>
2025-05-11 22:16:46 +0200

    wip: update user

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from init.controller import get_user_info, add_user, remove_user, update_user, get_map
-from init.model import users
+from untils.controller import get_user_info, add_user, remove_user, update_user, get_map
+from untils.models import users
+
 
 def main():
-
     while True:
-        print("==========MENU=================")
-        print("0 - zamknij aplikacje")
-        print("1 - wyświetl co u znajomych")
-        print("2 - dodaj nowego użytkownika")
-        print("3 - usuń użytkownika")
-        print("4 - edytuj użytkownika")
-        print("5 - przygotuj mapę znajomych")
-        print("==============MENU===================")
+        print("==============MENU================")
+        print('0 - zamknij aplikacje')
+        print('1 - wyswietl co u znajomych')
+        print('2 - dodaj nowego użytkownika')
+        print('3 - usuń użytkownika')
+        print('4 - edytuj uzytkownika')
+        print("5 - Przygotuj mapę znajomych")
+        print("==============MENU================")
 
-        choice = input("wybierz opcje menu")
+        choice = input('wybierz opcje menu')
         if choice == '0': break
         if choice == '1': get_user_info(users)
         if choice == '2': add_user(users)
@@ -21,6 +21,7 @@ def main():
         if choice == '4': update_user(users)
         if choice == '5': get_map(users)
 
-if _name_ == "_main_":
-    main()
 
+if __name__ == "_main_":
+    main()
+main()
--- untils/models.py
+++ untils/models.py
@@ -1,7 +1,7 @@
 users = [
     {"name": "Miłosz", "Location": "Warszawa", "posts": 500},
-    {"name": "Michał", "Location": "Krasnystaw", "posts": 400},
-    {"name": "Ksavier", "Location": "Grudziąc", "posts": 300},
+    {"name": "Michał", "Location": "Lublin", "posts": 400},
+    {"name": "Ksavier", "Location": "Wrocław", "posts": 300},
     {"name": "Damian", "Location": "Kraków", "posts": 200},
 
 ]
